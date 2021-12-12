00d8c8ad203c8f331e3cff46435bf754b7a57274
Tevien <sean.h.benson@googlemail.com>
2021-12-13 00:17:06 +0100

    improved logging and parsing

--- parser.py
+++ parser.py
@@ -4,6 +4,8 @@ import pandas as pd
 import re
 import pydicom
 import argparse
+import logging
+
 
 DEFAULT_DATE = datetime(2013, 1, 1)
 
@@ -45,7 +47,11 @@ def read_breast_mr_header(_f):
     if not ds['0018', '0015'].value == 'BREAST':
         return
 
-    loc = ds['0020', '1041'].value
+    try:
+        loc = ds['0020', '1041'].value
+    except KeyError:
+        logging.error(f"Failed at {_f}")
+        return
     #print(_f)
     #print(loc)
     return loc
@@ -55,6 +61,7 @@ def in_dicom_dir(_dir):
     _files = os.listdir(_dir)
 
     ret_val = False
+    low_pos = None
     file = ""
     if len(_files) < 10:
         return ret_val, file
@@ -62,10 +69,16 @@ def in_dicom_dir(_dir):
     for f in _files[:1]:
         pos = read_breast_mr_header(os.path.join(_dir, f))
         if pos:
-            print(f"{f}_{pos}")
-            ret_val = True
-            file = f
-            break
+            if not low_pos:
+                low_pos = pos
+            
+            if pos > low_pos:
+                continue
+            else:
+                #print(f"{f}_{pos}")
+                ret_val = True
+                file = f
+                low_pos = pos
     if ret_val:
         assert file, "First slice not found"
     return ret_val, file
@@ -78,24 +91,17 @@ if __name__ == '__main__':
     parse.add_argument('--output', default="processed.csv", type=str)
     args = parse.parse_args()
 
-    d_patient = {
-        'ID': [],
-        'Date': [],
-        'Description': [],
-        'Location': [],
-        'EchoTime': [],
-        'FlipAngle': [],
-        'Diff_b-val': [],
-        'Duration': [],
-    }
-
     patient_dir = args.input
     df_total = pd.DataFrame()
     patients = os.listdir(args.input)
+    log = args.output.replace(".csv", ".log")
+
+    logging.basicConfig(filename=log, level=logging.DEBUG)
+    
     skipped = []
     fixed_keys = None
 
-    for p in patients[:3]:
+    for p in patients:
         success = False
         for root, dirs, files in os.walk(os.path.join(patient_dir, p)):
             for d in dirs:
@@ -104,11 +110,13 @@ if __name__ == '__main__':
                     continue
                 else:
                     success = True
-                    print(f"{root} &&& {d} &&& {_first}")
+                    logging.info(50*"*")
+                    logging.info(f"{root} &&& {d} &&& {_first}")
+                    logging.info(50*"*")
 
                 # Found data point
                 point = {
-                    "Patient": p
+                    "Patient": [p]
                 }
 
                 full_loc = os.path.join(root, d, _first)
@@ -117,30 +125,35 @@ if __name__ == '__main__':
                 dates = valid_dates(str(path_to_dir).replace("/", " ").replace("MRI", " "))
                 assert len(dates) == 1, f"Need to do more refining, {dates}"
                 date = dates[0]
-                point["date"] = date.strftime("%m/%d/%Y")
-                point["location"] = full_loc
+                point["date"] = [date.strftime("%m/%d/%Y")]
+                point["location"] = [full_loc]
 
                 # Get header from first file
                 ds = pydicom.dcmread(full_loc)
 
                 # Remove pixel data key
                 keys = list(ds.keys())[:-1]
-                if not fixed_keys:
-                    fixed_keys = [k for k in keys if k.group<2000]
+                fixed_keys = [k for k in keys if k.group<2000]
 
                 # Add info to data point
                 for k in fixed_keys:
-                    try:
-                        v = ds[k]
-                    except:
-                        print(fixed_keys)
-                        v = ds[k]
-
-                    point[v.keyword] = v.value
+                    v = ds[k]
+                    if type(v.value) is list:
+                        #print(f"LIST FOUND - {v.keyword}")
+                        continue
+                    if type(v.value) is pydicom.sequence.Sequence:
+                        #print(f"SEQUENCE FOUND - {v.keyword}")
+                        continue
+                    point[v.keyword] = [str(v.value)]
+                    #print(f"ADDING: {v.keyword} - {str(v.value)}")
                 df = pd.DataFrame.from_dict(point)
                 #df_total = df_total.append(df, ignore_index=True)
-                df_total = pd.concat([df_total, df], axis=1)
+                if len(df_total):
+                    df_total = pd.concat([df_total, df])
+                else:
+                    df_total = df
         if not success:
             skipped.append(p)
     print(f"Skipped: {skipped}")
+    logging.warning(f"Skipped: {skipped}")
     df_total.to_csv(args.output, sep="\t")
